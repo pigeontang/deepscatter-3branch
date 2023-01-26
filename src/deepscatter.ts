e5b91fdd88df41c4aaeaf7f3605978786c18ebfb
Tongyu Ding <31868818+tongyuding@users.noreply.github.com>
2023-01-26 14:53:06 +0800

    updated

--- src/deepscatter.ts
+++ src/deepscatter.ts
@@ -299,12 +299,16 @@ export default class Scatterplot {
       corners.x = [-64473, 76698];
       corners.y = [-92314, 67908];
     }
+    console.log(corners);
     var xstep = (corners.x[1] - corners.x[0]) / xtimes;
     var ystep = (corners.y[1] - corners.y[0]) / xtimes;
-
+    
     const p: Promise<void> = new Promise((resolve, reject) => {
       for (let i = 0; i < xtimes; i++) {
         for (let j = 0; j < xtimes; j++) {
+          console.log([corners.x[0] + xstep * i, corners.x[0] + xstep * (i + 1)]);
+          console.log([corners.y[0] + ystep * j, corners.y[0] + ystep * (j + 1)]);
+
           setTimeout(() => {
             this._zoom.zoom_to_bbox(
               {
@@ -314,6 +318,7 @@ export default class Scatterplot {
               timeper / 5,
               1
             );
+            
             setTimeout(() => {
               this._renderer.fbos.colorpicker.use(() => {
                 this._renderer.render_all(this._renderer.props);
